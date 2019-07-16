a40296d916c759ac4b9b8d8fb8cdc8a56e3e4c54
Sergii Kasian <kasyansv1@gmail.com>
2019-07-16 15:20:45 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_people-table/precache-manifest.3d8c6b1e7697442ca25d7e26bf334fd1.js"
+  "/react_people-table/precache-manifest.5eab684bcb3f6ef5896fc8d541b5e702.js"
 );
 
 self.addEventListener('message', (event) => {
